bdf8aafececf5b2512f40412cf177ad53f183465
lance-l0t <sliceadice72@gmail.com>
2026-05-01 17:06:14 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// const advertBox = document.querySelector('.advert');
+
+// const images = [
+//   "assets/chad-montano-MqT0asuoIcU-unsplash.jpg",
+//   "assets/ivan-torres-MQUqbmszGGM-unsplash.jpg",
+//   "assets/joe-boshra-dZDXAjUnEVk-unsplash.jpg",
+//   "assets/joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg",
+//   "assets/victoria-shes-UC0HZdUitWY-unsplash.jpg",
+// ];
+
+// let i = 0;
+// function changeBg(){
+//     advertBox.style.opacity = '0';
+//     advertBox.style.transition = '.4s';
+//     if (i == images.length) {
+//         i = 0;
+//     }
+//     advertBox.style.backgroundImage = `url('${images[i]}')`;
+//     advertBox.style.opacity = '1';
+//     i++;
+//     console.log(images[i], i)
+//     setTimeout(changeBg , 6000);
+// }
+// changeBg();
